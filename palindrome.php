574b59c5762e72fde584d76515b99025d5f77515
chitraparmar1998 <83360789+chitraparmar1998@users.noreply.github.com>
2021-05-26 15:10:35 +0530

    D2
    day 2 work c program using php

--- palindrome.php
+++ palindrome.php
@@ -17,33 +17,3 @@ echo "$input is a Palindrome number";
 echo "$input is not a Palindrome";  
 }  
 ?>  
-Output:
-
-PHP Palindrome number 1
-Palindrome Number using Form in PHP
-Example:
-
-ADVERTISEMENT
-
-We'll show the logic to check whether a number is palindrome or not.
-
-<form method="post">        
-Enter a Number: <input type="text" name="num"/><br>  
-<button type="submit">Check</button>  
-</form>  
-<?php   
-    if($_POST)  
-    {  
-        //get the value from form  
-        $num = $_POST['num'];  
-        //reversing the number  
-        $reverse = strrev($num);  
-          
-        //checking if the number and reverse is equal  
-        if($num == $reverse){  
-            echo "The number $num is Palindrome";     
-        }else{  
-            echo "The number $num is not a Palindrome";   
-        }  
-}     
-      ?>  
